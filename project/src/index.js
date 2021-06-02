21e1635aab5837cb16b2e3b40e996e68e730c708
martinesidens <martines.idens@gmail.com>
2021-06-02 23:14:28 +0300

    02.06.2021 23:15

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import Main from './components/main/main';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
+  <Main />,
+  document.getElementById('root'),
+);

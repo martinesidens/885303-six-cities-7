62168fb56cb3be67927eee7eb1353baf6abdc7d4
вашÐьmartinesidens <martines.idens@gmail.com>
2021-06-21 23:22:22 +0300

    21.06.2021 23:25

--- project/src/components/card/card.jsx
+++ project/src/components/card/card.jsx
@@ -19,7 +19,7 @@ function Card(props) {
             <b className="place-card__price-value">&euro;120</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className="place-card__bookmark-button button" type="button">в
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
